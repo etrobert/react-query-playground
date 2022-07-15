7397bab232585cf082c8c34106267ac185e18526
erobert <etiennerobert33@gmail.com>
2022-07-15 14:29:40 +0200

    Add basic ReactQuery

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { useQuery } from 'react-query';
+
 import './App.css';
 
+const fetchData = () =>
+  new Promise<string>((resolve) => {
+    console.log('Fetching Data');
+    resolve('myData');
+  });
+
 function App() {
+  const { data } = useQuery('data', fetchData);
   return (
     <div className="App">
-      <header className="App-header">Hello World!</header>
+      <header className="App-header">{data}</header>
     </div>
   );
 }
